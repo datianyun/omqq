d31abd5fed79eee9699d7fcfadf147d262a155c8
tianyun <yuntian@tencent.com>
2016-05-18 18:47:20 +0800

    提交最基本的gulp命令供开发使用

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# omqq
+# redux 练习
+
+### 初始化
+
+``````javascript
+
+ npm install
+
+``````
+
+### 实例
+
+``````javascript
+
+ npm start
+
+ http://localhost:3000 是一个todo 实例
+
+``````
+### 打包
+
+``````javascript
+
+  gulp  build
+
+
+``````
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,28 @@
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
 var webConfig = require('./webpack.config.js');
-gulp.task('default', function() {
-  return gulp.src('./index.js')
-  .pipe(webpack(webConfig))
-  .pipe(gulp.dest('dist'));
+var del = require('del');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+
+//清理文件
+gulp.task('clean', function(){
+	del('./dist/');
 });
+
+//打包react
+gulp.task('webpack', function() {
+  return gulp.src('./src/js/entry')
+  .pipe(webpack(webConfig))
+  .pipe(gulp.dest('dist/js'));
+});
+
+//压缩CSS
+gulp.task('cssmin', function () {
+	gulp.src('src/css/*.css')
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build', ['clean','webpack','cssmin']);
