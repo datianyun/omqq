3fb6dcde1a31090b9ab1edad6ad0f8cff0f2fdaa
云 田 <yuntian@yundeMacBook-Pro.local>
2016-07-18 15:20:28 +0800

    修改server.js

--- server.js
+++ server.js
@@ -9,29 +9,20 @@ var port = 3000
 var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler, {
-  path: '/__webpack_hmr',
-  heartbeat: 10 * 1000
+    path: '/__webpack_hmr',
+    heartbeat: 10 * 1000
 }));
 
 app.use('/static', express.static('dist'));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/src/html/index.html')
-})
-
-
-app.get("/login", function(req, res) {
-  res.sendFile(__dirname + '/src/html/login.html')
-})
-
-app.get("/system", function(req, res) {
-  res.sendFile(__dirname + '/src/html/system.html')
+    res.sendFile(__dirname + '/src/html/index.html')
 })
 
 app.listen(port, function(error) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-  }
+    if (error) {
+        console.error(error)
+    } else {
+        console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    }
 })
